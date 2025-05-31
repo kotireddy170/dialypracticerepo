139aa754ae20287619069949cfb5290e5c35239c
kotiReddy <kotireddycivil123@gmail.com>
2025-05-31 16:23:39 +0530

    username added details

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "please enter your username::;
-read USERNAME 
+echo "please enter your username::; # Text enter here will be added as value to variable 
+read USERNAME # USERNAME is the variable name   
